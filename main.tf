e0301163c4bb9a5949391ec98c4e6406578ac721
Yaniv <yaniv.raines@gmail.com>
2025-09-14 13:41:47 +0000

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+# The configuration for the `remote` backend.
+backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Yaniv-Practice"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+        name = "example-workspace"
+    }
+}
+
+
+resource "null_resource" "example" {
+    triggers = {
+        value = "A example resource that does nothing!"
+    }
+}
